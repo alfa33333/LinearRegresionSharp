41b4ecc67ce1a60ba1310aada5d99be8de720890
alfa33333 <44150484+alfa33333@users.noreply.github.com>
2024-08-25 23:48:17 -0400

    try to undersrtand how to read the csv file as data

--- DataRead/Program.cs
+++ DataRead/Program.cs
@@ -1,6 +1,27 @@
-﻿using Microsoft.Data.Analysis;
+﻿using System.Globalization;
+using Microsoft.Data.Analysis;
+using CsvHelper;
 
-
-const string dataPath = @"../../../../Data/example.csv";
+const string dataPath = @"C:\Users\alfa33333\Documents\GitHub\LinearRegresionSharp\Data\example.csv";
 
 var dataFrame = DataFrame.LoadCsv(dataPath);
+
+Console.WriteLine(dataFrame);
+
+using var reader = new StreamReader(dataPath);
+using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+{
+    var records = csv.GetRecords<Foo>();
+    foreach (var record in records)
+    {
+        Console.WriteLine(record.X);
+    }
+}
+
+
+
+public class Foo
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+}
