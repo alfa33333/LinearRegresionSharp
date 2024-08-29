bc237c9c60749db4572548d8f6314edb6b89351c
alfa33333 <44150484+alfa33333@users.noreply.github.com>
2024-08-28 21:24:36 -0400

    Add reading class and test

---
+++ LinearRegression/Linear.cs
@@ -0,0 +1,10 @@
+
+
+namespace LinearRegression;
+
+
+
+public class LinearModel
+{
+
+}
--- LinearRegression/Program.cs
+++ LinearRegression/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using LinearRegression;
+using DataRead;
 
-Console.WriteLine("Hello, World!");
+const string dataPath = @"../../../../Data/example.csv";
+
+DataPack exampleData = new DataPack();
+
+exampleData.DataReader(dataPath);
+
+
+
+// Get the properties of the object
+var properties = exampleData.GetType().GetProperties();
+
+// Iterate over the properties and print their names
+Console.WriteLine($"{properties[0].Name} {properties[1].Name}");
+
+
+for (int i = 0; i < exampleData.X.Count; i++)
+{
+    Console.WriteLine($"{exampleData.X[i]}  {exampleData.Y[i]}");
+}
