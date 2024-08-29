b0cf664181203ec2bdcddddfefbcb178eb0231b7
alfa33333 <44150484+alfa33333@users.noreply.github.com>
2024-08-28 21:11:40 -0400

    ignore several local files, and add namespace and connect both projects

--- DataRead/Program.cs
+++ DataRead/Program.cs
@@ -2,6 +2,7 @@
 using System.Reflection;
 using Microsoft.Data.Analysis;
 using CsvHelper;
+using DataRead;
 
 const string dataPath = @"C:\Users\alfa33333\Documents\GitHub\LinearRegresionSharp\Data\example.csv";
 
@@ -16,30 +17,33 @@ dataPack.DataReader(dataPath);
 dataPack.X.ForEach(Console.WriteLine);
 dataPack.Y.ForEach(Console.WriteLine);
 
-public class DataPack
+namespace DataRead
 {
-    public List<float> X { get; set; } = [];
-    public List<float> Y { get; set; } = [];
-
-    public void DataReader(string dataPath)
+    public class DataPack
     {
-        using var reader = new StreamReader(dataPath);
-        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
-        csv.Read();
-        csv.ReadHeader();
-        string[]? headers = csv.HeaderRecord;
-        ArgumentNullException.ThrowIfNull(headers);
-        while (csv.Read())
+        public List<float> X { get; set; } = [];
+        public List<float> Y { get; set; } = [];
+
+        public void DataReader(string dataPath)
         {
-            foreach (var header in headers)
+            using var reader = new StreamReader(dataPath);
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            csv.Read();
+            csv.ReadHeader();
+            string[]? headers = csv.HeaderRecord;
+            ArgumentNullException.ThrowIfNull(headers);
+            while (csv.Read())
             {
-                if (header == "Y")
-                    Y.Add(csv.GetField<float>(header));
-                else
-                    X.Add(csv.GetField<float>(header));
+                foreach (var header in headers)
+                {
+                    if (header == "Y")
+                        Y.Add(csv.GetField<float>(header));
+                    else
+                        X.Add(csv.GetField<float>(header));
+                }
             }
         }
+
+
     }
-
-
 }
