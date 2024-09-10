20ec75e4f5ecad2f2aafe3348cdc6040b1eb948c
alfa33333 <44150484+alfa33333@users.noreply.github.com>
2024-09-09 20:41:46 -0400

    Add ReadData class for CSV and DataFrame data reading
    Introduce ReadData class in RegressionApp.Models to read data from CSV files and DataFrame objects into lists X and Y. Add methods DataReader(string dataPath) and DataReader(DataFrame dataFrame) for data reading. Update project files to include CsvHelper package version 33.0.1 and necessary metadata.

---
+++ RegressionApp/Models/ReadData.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Data.Analysis;
+using CsvHelper;
+using System.Globalization;
+
+namespace RegressionApp.Models
+{
+    public class ReadData
+    {
+        public List<float> X { get; set; } = [];
+        public List<float> Y { get; set; } = [];
+
+        public void DataReader(string dataPath)
+        {
+            using var reader = new StreamReader(dataPath);
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            csv.Read();
+            csv.ReadHeader();
+            string[]? headers = csv.HeaderRecord;
+            ArgumentNullException.ThrowIfNull(headers);
+            while (csv.Read())
+            {
+                foreach (var header in headers)
+                {
+                    if (header == "Y")
+                        Y.Add(csv.GetField<float>(header));
+                    else
+                        X.Add(csv.GetField<float>(header));
+                }
+            }
+        }
+
+        public void DataReader(DataFrame dataFrame)
+        {
+            foreach (var column in dataFrame.Columns)
+            {
+                if (column.Name == "Y")
+                {
+                    foreach (float item in column)
+                    {
+                        Y.Add(item);
+                    }
+                }
+                else
+                {
+                    foreach (float item in column)
+                    {
+                        X.Add(item);
+                    }
+                }
+            }
+        }
+    }
+}
