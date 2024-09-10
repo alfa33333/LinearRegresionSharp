4e91ce0cd6403676c5ec5757a62ba959d4cf241f
alfa33333 <44150484+alfa33333@users.noreply.github.com>
2024-09-09 20:53:10 -0400

    Will add randomized button and data

--- RegressionApp/obj/Debug/net8.0/RegressionApp.AssemblyInfo.cs
+++ RegressionApp/obj/Debug/net8.0/RegressionApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RegressionApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ba8e1f3d6e6de546bd1bbf1baba9a8cab6e6bb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20ec75e4f5ecad2f2aafe3348cdc6040b1eb948c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RegressionApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RegressionApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
